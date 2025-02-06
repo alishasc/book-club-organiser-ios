d69597519028999ae92543364e67daae83579fb5
Alisha Carrington <alishasc2001@icloud.com>
2025-02-06 21:05:44 +0000

    first commit. user authentication and onboarding working. updated profile page with user details.

---
+++ BookClub/Views/Events/EventDatesView.swift
@@ -0,0 +1,18 @@
+//
+//  EventDatesView.swift
+//  BookClub
+//
+//  Created by Alisha Carrington on 01/02/2025.
+//
+
+import SwiftUI
+
+struct EventDatesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EventDatesView()
+}
