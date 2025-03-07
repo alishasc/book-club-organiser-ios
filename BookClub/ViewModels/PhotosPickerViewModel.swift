bf46fae7093fe80cae7c21bc581c238b5b52ae33
Alisha Carrington <alishasc2001@icloud.com>
2025-03-07 16:48:55 +0000

    added photo picker for book club cover image. can upload and load image from cloud storage. need to fix where these functions are called

---
+++ BookClub/ViewModels/PhotosPickerViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PhotosPickerViewModel.swift
+//  BookClub
+//
+//  Created by Alisha Carrington on 05/03/2025.
+//
+
+import Foundation
---
+++ BookClub/Views/testPhoto.swift
@@ -0,0 +1,18 @@
+//
+//  testPhoto.swift
+//  BookClub
+//
+//  Created by Alisha Carrington on 05/03/2025.
+//
+
+import SwiftUI
+
+struct testPhoto: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    testPhoto()
+}
